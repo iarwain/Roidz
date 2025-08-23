a9e8c3dd62a5460c4ed1258bd2a89a45c2a71d99
iarwain <iarwain@orx-project.org>
2025-08-23 22:18:17 +0200

    - Updated to latest version of orx/Scroll
    - Fixed error introduced in previous commit

--- src/Ship.cpp
+++ src/Ship.cpp
@@ -15,10 +15,10 @@ void Ship::OnDelete()
 
 void Ship::Update(const orxCLOCK_INFO &_rstInfo)
 {
-  PushConfigSection();
-
   // Update base object
   Object::Update(_rstInfo);
+
+  PushConfigSection();
 
   // Rotate
   SetAngularVelocity(orxMATH_KF_DEG_TO_RAD * orxConfig_GetFloat("TurnSpeed") * (orxInput_GetValue("RotateCW") - orxInput_GetValue("RotateCCW")));
